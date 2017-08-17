2479673ebcae4c9a0d63c0aa288a86bbb7cc56b9
ee92 <eegor650@gmail.com>
2017-08-17 16:25:11 -0700

    test for logout route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -257,3 +257,23 @@ describe('POST /users/login', () => {
       .end(done);
   });
 });
+
+// test for user logout
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    supertest(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
