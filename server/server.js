ba3a3e584761cbe93c968c4e274cf2ee2a263f17
ee92 <eegor650@gmail.com>
2017-08-17 16:21:34 -0700

    DELETE route for logout

--- server/server.js
+++ server/server.js
@@ -138,6 +138,14 @@ app.post('/users/login', (req, res) => {
   });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  })
+});
+
 // specifiy port for express
 app.listen(port, () => {
   console.log("listening on port " + port);
